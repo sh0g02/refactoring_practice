dea640bd353caf80d5082b093d311e8faa013777
takasaki <takasaki@inagora.com>
2021-01-12 16:28:29 +0900

    各種合計メソッドを問い合わせ関数として独立させる

--- src/customer.rb
+++ src/customer.rb
@@ -11,22 +11,25 @@ class Customer
   end
 
   def statement
-    total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
 
-    # 各行の金額計算
     @rentals.each do |element|
-      # レンタルポイント加算
-      frequent_renter_points += element.frequent_renter_points
-
       # このレンタルの料金を表示
       result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
-      total_amount += element.charge
     end
 
     # フッター行追加
-    result += "Amount owed is #{total_amount}\n"
-    result += "You earned #{frequent_renter_points} frequent renter points"
+    result += "Amount owed is #{total_charge}\n"
+    result += "You earned #{total_frequent_renter_points} frequent renter points"
     result
+  end
+
+  private
+  def total_charge
+    @rentals.sum {|rental| rental.charge}
+  end
+
+  def total_frequent_renter_points
+    @rentals.sum {|rental| rental.frequent_renter_points}
   end
 end
